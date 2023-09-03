eae1b43915dcd658287554d2a2b81fb817bcbe73
sanket9930 <sankettiwari1847@gmail.com>
2023-09-03 22:50:42 +0530

    Modified the code

--- API/app.js
+++ API/app.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://sanket:Sanket1234@surf-shop.snbyqbb.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
